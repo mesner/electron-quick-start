3f594b1a6c8dcdad61adf03d794992f43aef889d
Ian German Mesner <ian@chartiq.com>
2019-10-28 15:49:39 -0400

    protocol handling 2x

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, protocol} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
+} 
+
+protocol.registerSchemesAsPrivileged([
+  { scheme: 'iantest2', privileges: { standard: true, secure: true } },
+])
+
+if(!app.isDefaultProtocolClient('iantest')) {
+  let reg = app.setAsDefaultProtocolClient('iantest')
+  console.log(`setAsDefaultProtocolClient ${reg}`)
+}
+
+
 function createWindow () {
+
+  
+  protocol.registerStringProtocol('iantest2', (request, callback) => {
+    console.log(request.url)
+    callback(`Hello from ${request.url}`)
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  })
+
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800,
@@ -17,13 +42,16 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  //mainWindow.loadFile('index.html')
+  mainWindow.loadURL('http://127.0.0.1:8080/index.html')
+  
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
+    app.removeAsDefaultProtocolClient('iantest')
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -36,11 +64,17 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
+app.on('second-instance', (event, commandLine, workingDirectory) => {
+  console.log(`second-instance ${commandLine}, ${workingDirectory}`)
+  createWindow()
+})
+
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') app.quit()
+
 })
 
 app.on('activate', function () {
